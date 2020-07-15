c838627ecb538cde085d7f73bdfc56b3468db34e
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-15 18:31:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Sunny Kumar");
+$("#policyNumber").text("RML1200755");
+$("#startDate").text("16-Jul-2020");
+$("#endDate").text("15-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB07BK9273");
+$("#make").text("HERO MOTOCORP");
+$("#model").text("SPLENDOR PLUS");
+$("#vehicalSubType").text("SELF");
+$("#engineNo").text("HA10AGHHJB0704");
+$("#chassis").text("MBLHAR080HHJ11767");
